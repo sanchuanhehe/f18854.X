bb0a9a7974c250b4297aec9dda63bede6f040d98
wangyihe <heheyoumingwyh@qq.com>
2024-07-11 11:44:19 +0800

    Remove folders from Git while keeping local copies

--- .generated-cmakefiles/f18854/default/image_default.elf/file.cmake
+++
@@ -1,8 +0,0 @@
-set(FILE_GROUP_assemble "../../../../pic_8b_asm_func.s")
-set(FILE_GROUP_assemblePreprocess "")
-set(FILE_GROUP_compile "")
-set(FILE_GROUP_link "")
-set(BUILD_LIBRARY 0)
-set(internal_image_name "c:/Users/hehey/MPLABXProjects/f18854.X/_build_mplab/f18854/default/image_default.elf/default.elf")
-set(image_name "default.elf")
-set(output_dir "c:/Users/hehey/MPLABXProjects/f18854.X/out/f18854")
--- build/default/production/pic_8b_asm_func.o.d
+++
@@ -1,2 +0,0 @@
-build/default/production/pic_8b_asm_func.i:  \
-pic_8b_asm_func.s 
