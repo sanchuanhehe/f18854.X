c3b443f1ad72ae7e4fd2d1b7a9b8d5575fdfd102
wangyihe <heheyoumingwyh@qq.com>
2024-07-19 13:25:20 +0800

    refactor: Remove unnecessary extern declaration for eusart_receive_buffer

--- eusart.h
+++ eusart.h
@@ -1,7 +1,9 @@
-# ifndef _EUSART_H
-# define _EUSART_H
+#ifndef _EUSART_H
+#define _EUSART_H
 
-# include <xc.h>
+#include <stdint.h>
+#include <xc.h>
+
 
 extern char eusart_receive_buffer;
 
@@ -9,4 +11,4 @@ void init_eusart_func(void);
 void eusart_tx_func(char data);
 void eusart_rx_func(void);
 
-# endif
+#endif
